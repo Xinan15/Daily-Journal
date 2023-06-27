34d4f0b8411ea51cf9f1cf1c03124449a86fefbc
Xinan <youkingan11@gmail.com>
2023-06-27 09:55:35 +0100

    challenge 19

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+// Lodash
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -15,7 +17,7 @@ const app = express();
 
 let posts = [];
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -52,6 +54,25 @@ app.post("/compose", function (req, res) {
 
   posts.push(post);
   res.redirect("/");
+});
+
+// Express: route parameters
+
+app.get("/posts/:postName", function (req, res) {
+
+  // Lodash
+  const requestedTitle=_.lowerCase(req.params.postName);
+
+  posts.forEach(function(post){
+    const storedTitle=_.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle){
+      res.render("post",{
+        title:post.title,
+        content:post.content})
+    }
+
+  })
 });
 
 app.listen(3000, function () {
